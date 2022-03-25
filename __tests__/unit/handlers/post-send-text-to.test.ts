fa13690ea845a2075f9cb5d7026413b90dfc9d93
David Bowland <dbowland1@gmail.com>
2022-03-25 11:24:06 -0500

    Add send text to endpoint

---
+++ __tests__/unit/handlers/post-send-text-to.test.ts
@@ -0,0 +1,52 @@
+import { decodedJwt } from '../__mocks__'
+import eventJson from '@events/post-send-text-to.json'
+import { postSendTextToHandler } from '@handlers/post-send-text-to'
+import { mocked } from 'jest-mock'
+import * as queue from '@services/queue'
+import { APIGatewayProxyEventV2 } from '@types'
+import * as events from '@utils/events'
+import status from '@utils/status'
+
+jest.mock('@services/queue')
+jest.mock('@utils/events')
+jest.mock('@utils/logging')
+
+describe('post-send-text-to', () => {
+  const event = eventJson as unknown as APIGatewayProxyEventV2
+
+  beforeAll(() => {
+    mocked(events).extractJwtFromEvent.mockReturnValue(decodedJwt)
+    mocked(queue).sendSms.mockResolvedValue(undefined)
+  })
+
+  describe('postSendTextToHandler', () => {
+    test('expect INTERNAL_SERVER_ERROR when extractJwtFromEvent throws', async () => {
+      mocked(events).extractJwtFromEvent.mockImplementationOnce(() => {
+        throw new Error('JWT error')
+      })
+      const result = await postSendTextToHandler(event)
+      expect(result).toEqual(expect.objectContaining(status.INTERNAL_SERVER_ERROR))
+    })
+
+    test('expect FORBIDDEN when JWT is invalid', async () => {
+      mocked(events).extractJwtFromEvent.mockReturnValueOnce(null)
+      const result = await postSendTextToHandler(event)
+      expect(result).toEqual({ statusCode: 403, body: JSON.stringify({ message: 'Invalid JWT' }) })
+    })
+
+    test('expect BAD_REQUEST when invalid phone number', async () => {
+      const tempEvent = { ...event, pathParameters: { sessionId: 'abc123', toUserId: '+invalid' } }
+      const result = await postSendTextToHandler(tempEvent)
+      expect(result).toEqual({ ...status.BAD_REQUEST, body: JSON.stringify({ message: 'Invalid phone number' }) })
+    })
+
+    test('expect sendSMS is called and NO_CONTENT status returned', async () => {
+      const result = await postSendTextToHandler(event)
+      expect(mocked(queue).sendSms).toHaveBeenCalledWith(
+        '+18005556789',
+        'Dave (+15551234567) is inviting you to vote: http://choosee.bowland.link/s/abc123?u=%2B18005556789'
+      )
+      expect(result).toEqual(status.NO_CONTENT)
+    })
+  })
+})
